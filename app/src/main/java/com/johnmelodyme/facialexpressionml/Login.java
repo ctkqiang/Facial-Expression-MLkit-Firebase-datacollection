223407a5c06773a0d1e5cffaff1245c03adc258e
John Melody Me, Tan Sin Dee <johnmelodyme@icloud.com>
2020-02-01 14:41:27 +0800

    <Authentication>(Register)completed; <Authentication>(Login)added;

--- app/src/main/java/com/johnmelodyme/facialexpressionml/Login.java
+++ app/src/main/java/com/johnmelodyme/facialexpressionml/Login.java
@@ -1,10 +1,23 @@
 package com.johnmelodyme.facialexpressionml;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * @Author : John Melody Melissa
+ * @Copyright: John Melody Melissa  © Copyright 2020
+ * @INPIREDBYGF : Sin Dee <3
+ */
+
 public class Login extends AppCompatActivity {
+    private static final String TAG = Login.class.getName();
+    private Button LOGIN;
+    private EditText EMAIL, PASSWORD;
+    private TextView FORGOTPASSWORD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/johnmelodyme/facialexpressionml/Register.java
+++ app/src/main/java/com/johnmelodyme/facialexpressionml/Register.java
@@ -1,22 +1,25 @@
 package com.johnmelodyme.facialexpressionml;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import cn.pedant.SweetAlert.SweetAlertDialog;
+
+import dmax.dialog.SpotsDialog;
 
 /**
  * @Author : John Melody Melissa
@@ -30,6 +33,10 @@ public class Register extends AppCompatActivity {
     private TextView EXISTING, FORGOT_PASS;
     private EditText EMAIL, PASSWORD;
     private FirebaseAuth FIREBASEAUTH;
+    private LayoutInflater LAYOUTINFLATER;
+    private View VIEW;
+    private AlertDialog.Builder PROMPT;
+    private android.app.AlertDialog ALERT_PROMPT, ALERT_RESET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,17 @@ public class Register extends AppCompatActivity {
         PASSWORD = findViewById(R.id.password);
         FORGOT_PASS = findViewById(R.id.forgotpass);
         FIREBASEAUTH = FirebaseAuth.getInstance();
+        LAYOUTINFLATER = LayoutInflater.from(Register.this);
+        VIEW = LAYOUTINFLATER.inflate(R.layout.forgotpassword, null);
+        PROMPT = new AlertDialog.Builder(Register.this);
+
+        ALERT_PROMPT = new SpotsDialog
+                .Builder()
+                .setContext(Register.this)
+                .setMessage("Registering.....")
+                .setCancelable(false)
+                .build();
+
 
         EXISTING.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,11 +96,7 @@ public class Register extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()){
-                                SweetAlertDialog pDialog = new SweetAlertDialog(Register.this, SweetAlertDialog.PROGRESS_TYPE);
-                                pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
-                                pDialog.setTitleText("Loading.....");
-                                pDialog.setCancelable(false);
-                                pDialog.show();
+                                ALERT_PROMPT.show();
                                 Intent TOMAIN;
                                 TOMAIN = new Intent(Register.this, MainActivity.class);
                                 startActivity(TOMAIN);
@@ -102,7 +116,50 @@ public class Register extends AppCompatActivity {
         FORGOT_PASS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final EditText EMAIL_res;
+                final String Reset_Email, Rem, yes, err;
 
+                EMAIL_res = VIEW.findViewById(R.id.reset_pass_email);
+                Rem = getResources().getString(R.string.itsok);
+                yes = getResources().getString(R.string.rpass);
+                err = getResources().getString(R.string.pls);
+                Reset_Email = EMAIL_res.getText()
+                        .toString()
+                        .trim();
+                ALERT_RESET = new SpotsDialog
+                        .Builder()
+                        .setContext(Register.this)
+                        .setMessage("Reset Email sent to  " + Reset_Email)
+                        .setCancelable(false)
+                        .build();
+                PROMPT.setView(VIEW);
+                PROMPT.setNegativeButton(Rem, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogue, int ID) {
+                        Log.w(TAG, "FE " + "User Suddenly, magically remembered his/her password");
+                    }
+                }).setPositiveButton(yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogue, int ID) {
+                        if (TextUtils.isEmpty(Reset_Email)){
+                            EMAIL_res.setError(err);
+                        } else {
+                            FIREBASEAUTH.sendPasswordResetEmail(Reset_Email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        ALERT_RESET.show();
+                                        ROTIBAKAR("Password Reset Email Sent To " + Reset_Email);
+                                    } else {
+                                        ROTIBAKAR("FAILED");
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+                AlertDialog alertDialog = PROMPT.create();
+                alertDialog.show();
             }
         });
     }
