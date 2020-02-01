5ee08ee2905d22bbe19ae69bcf31f14d753fab41
John Melody Me, Tan Sin Dee <Johnmelodyme@icloud.com>
2020-02-01 10:42:44 +0800

    Uploaded, Committed

---
+++ app/src/main/java/com/johnmelodyme/facialexpressionml/Authentication/Login.java
@@ -0,0 +1,16 @@
+package com.johnmelodyme.facialexpressionml.Authentication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.johnmelodyme.facialexpressionml.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
