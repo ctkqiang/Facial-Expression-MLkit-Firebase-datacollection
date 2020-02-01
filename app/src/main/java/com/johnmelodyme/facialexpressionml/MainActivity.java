76141bca13166bf598fdacadc981b8f75ba3069c
John Melody Me, Tan Sin Dee <Johnmelodyme@yandex.com>
2020-02-01 10:16:37 +0800

    Uploaded, Committed

---
+++ app/src/main/java/com/johnmelodyme/facialexpressionml/MainActivity.java
@@ -0,0 +1,18 @@
+package com.johnmelodyme.facialexpressionml;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @Author : John Melody Melissa
+ * @Copyright: John Melody Melissa  © Copyright 2020
+ * @INPIREDBYGF
+ */
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
